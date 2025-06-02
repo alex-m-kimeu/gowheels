d4f4d72602e98f291b9209a2198bad27aea6628b
Alex Kimeu <alexkimeu1999@gmail.com>
2025-06-02 11:47:32 +0300

    Added a why choose us section

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { searchSchema, type SearchFormData } from '@/lib/validations';
-import { Hero } from '@/components/home/Hero';
+import { Hero, WhyChooseUs } from '@/components/home';
 
 const Home = () => {
 	const form = useForm<SearchFormData>({
@@ -22,8 +22,9 @@ const Home = () => {
 	};
 
 	return (
-		<main className='flex flex-col items-center bg-[var(--white)] gap-[24px] md:gap-[32px] lg:gap-[40px] xl:gap-[48px] 2xl:gap-[56px]'>
+		<main className='flex flex-col items-center bg-[var(--white)] gap-[32px] lg:gap-[40px] xl:gap-[48px] 2xl:gap-[56px]'>
 			<Hero form={form} onSubmit={onSubmit} />
+			<WhyChooseUs />
 		</main>
 	);
 };
